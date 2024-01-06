e284fbcb9808f5d0ff5cc2e62fa402dee6657213
ayoubjbili <ayoubjbili@gmail.com>
2024-01-06 17:00:33 +0100

    fixed small devices

--- src/models/Model.jsx
+++ src/models/Model.jsx
@@ -496,13 +496,8 @@ const Model = () => {
           onClick={async () => {
             const modelViewer = document.getElementById("model1");
             // Check if AR is supported
-            if (window.screen.width<768) {
-              if(modelViewer.canActivateAR)
-                await modelViewer.activateAR()
-              else {
-                document.getElementById("custom-alert").style.display = "block";
-                console.error('AR not supported.');
-              }
+            if (modelViewer.canActivateAR) {
+              await modelViewer.activateAR();
             } else {
               document.getElementById("custom-alert").style.display = "block";
               console.error('AR not supported.');
